5191429c3d50691a16ce3c29369fbd84badc3af1
josh <joshrachner@josh-rachners-macbook-pro-2.local>
2011-04-24 18:42:18 -0500

    fix

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,5 +2,5 @@ class Content < ActiveRecord::Base
   named_scope :get_welcome, :conditions => "location = 'welcome'"
   named_scope :get_employment, :conditions => "location = 'employment'"
   named_scope :get_mission, :conditions => "location = 'mission'"
-  named_scope :get_technologies, :conditions => "location = 'technology'"
+  named_scope :get_technologies, :conditions => "location = 'technologies'"
 end
