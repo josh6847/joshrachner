e810cf1d66ae6566d76291ae76498d19284cc3a3
josh <joshrachner@josh-rachners-macbook-pro-3.local>
2011-07-10 13:21:16 -0500

    blog int

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   require 'rss/1.0'
   require 'rss/2.0'
+  require 'nokogiri'
   require 'open-uri'
   
   def index
@@ -9,18 +10,6 @@ class HomeController < ApplicationController
     content = "" # raw content of rss feed will be loaded here
     open(source) do |s| content = s.read end
     @rss = RSS::Parser.parse(content, false)
-    #puts "Root values"
-    #print "RSS title: ", rss.channel.title, "\n"
-    #print "RSS link: ", rss.channel.link, "\n"
-    #print "RSS description: ", rss.channel.description, "\n"
-    #print "RSS publication date: ", rss.channel.date, "\n"
-    #
-    #puts "Item values"
-    #print "number of items: ", rss.items.size, "\n"
-    #print "title of first item: ", rss.items[0].title, "\n"
-    #print "link of first item: ", rss.items[0].link, "\n"
-    #print "description of first item: ", rss.items[0].description, "\n"
-    #print "date of first item: ", rss.items[0].date, "\n"
     @page_title = "Home"
   end
 
@@ -38,6 +27,10 @@ class HomeController < ApplicationController
 
   def blog
     @blog = "selected"
+    source = "http://blog.joshrachner.com/feed/" # url or local file
+    content = "" # raw content of rss feed will be loaded here
+    open(source) do |s| content = s.read end
+    @rss = RSS::Parser.parse(content, false)
   end
   
   def method_missing(*args)
